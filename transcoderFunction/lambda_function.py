a5ced53efdfe4fcb0dd03d94bfed2537de5c9ea8
oymak.oymak <oymak.oymak@klarna.com>
2020-08-13 10:49:28 +0200

    Transcoder function working

--- transcoderFunction/lambda_function.py
+++ transcoderFunction/lambda_function.py
@@ -8,6 +8,7 @@ import re
 import urllib.parse
 import uuid
 import subprocess
+
 # import moviepy.editor as mp
 
 s3 = boto3.client('s3')
@@ -30,20 +31,20 @@ def get_media_format(path):
     elif re.search('.ogg$', path) is not None:
         return 'ogg'
     else:
-        return 'mp3'
+        return 'mp4'
 
 
 def get_s3_metadata(bucket, key):
+    print(f"Getting metadata for: {bucket}/{key}")
     return s3.head_object(Bucket=bucket, Key=key)['Metadata']
 
 
 def lambda_handler(event, context):
-
     # Generate a unique name for the job
     bucket_name = event['Records'][0]['s3']['bucket']['name']
     _object_key = event['Records'][0]['s3']['object']['key']
     object_key = urllib.parse.unquote_plus(_object_key)
-    uniqueId= str(uuid.uuid4())
+    uniqueId = str(uuid.uuid4())
     filenameWithoutExt = os.path.splitext(object_key)[0]
     soundFileName = f"{uniqueId}-sound-{filenameWithoutExt}"
     videoFileName = f"{uniqueId}-video-{object_key}"
@@ -57,16 +58,27 @@ def lambda_handler(event, context):
     # Language list: en-US | es-US | en-AU | fr-CA | en-GB | de-DE | pt-BR | fr-FR | it-IT | ko-KR | es-ES | en-IN | hi-IN | ar-SA | ru-RU | zh-CN | nl-NL | id-ID | ta-IN | fa-IR | en-IE | en-AB | en-WL | pt-PT | te-IN | tr-TR | de-CH | he-IL | ms-MY | ja-JP | ar-AE
     # See https://docs.aws.amazon.com/transcribe/latest/dg/API_StartTranscriptionJob.html for the most up-to-date list of languages available.
     language_code = media_metadata.get('languagecode', 'en-US')
-    max_speaker_labels = int(media_metadata.get('maxspeakerlabels', '1'))
+    max_speaker_labels = media_metadata.get('maxspeakerlabels', '1')
 
-
+    print(f"Dowloading video file to efs: {efsPath}/{videoFileName}")
     s3.download_file(Bucket=bucket_name, Key=object_key, Filename=f"{efsPath}/{videoFileName}")
 
     # clip = mp.VideoFileClip(f"{efsPath}/{videoFileName}")
     # clip.audio.write_audiofile(f"{efsPath}/{soundFileName}.mp3")
-    subprocess.call(['ffmpeg', '-i', f"{efsPath}/{videoFileName}", '-vn', '-acodec', 'copy', f"{efsPath}/{soundFileName}.mp3"])
+    print(f"Calling subprocess ffmpeg with {efsPath}/{videoFileName} to {efsPath}/{soundFileName}.mp3")
+    subprocess.call(['/opt/bin/ffmpeg', '-loglevel', 'error', '-i', f"{efsPath}/{videoFileName}", '-vn',
+                     f"{efsPath}/{soundFileName}.mp3"])
     with open(f"{efsPath}/{soundFileName}.mp3", 'rb') as f:
         data = f.read()
-        s3.Object(audioOutputBucket, f"{soundFileName}.mp3").put(Body=data, Metadata={'languagecode': language_code,
-                                                                                      'channelidentification': channel_identification,
-                                                                                      'maxspeakerlabels': max_speaker_labels})
+        print("Reading transformed file")
+        s3.put_object(Bucket=audioOutputBucket, Key=f"{soundFileName}.mp3", Body=data,
+                      Metadata={'languagecode': language_code,
+                                'channelidentification': channel_identification,
+                                'maxspeakerlabels': max_speaker_labels})
+
+        print(
+            f"Put audio output to {audioOutputBucket} with {soundFileName}.mp3 and metadata:"
+            f" {language_code},{channel_identification},{max_speaker_labels} ")
+    print("Cleaning up efs temp files...")
+    os.remove(f"{efsPath}/{soundFileName}.mp3")
+    os.remove(f"{efsPath}/{videoFileName}")
